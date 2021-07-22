2690bd103e8c054a68962be696f2e013262da632
alexzhu25 <alexzhu25@gmail.com>
2021-07-22 16:47:34 -0700

    Powershell scripts folder and app removal script
    Added powershell scripts folder.
    
    In folder, added powershell script to remove 3dviewer and paint3d.

---
+++ powershell scripts/script to remove 3dviewer and paint3d.ps1
@@ -0,0 +1,25 @@
+﻿# Powershell script for removing 3D Viewer and Paint 3D from a computer
+# Some packages are hard-coded. Update as necessary.
+
+Write-Host "Removing provisioned package for 3D Viewer"
+try {Remove-AppxProvisionedPackage -Online -PackageName Microsoft.Microsoft3DViewer_2021.2105.4012.0_neutral_~_8wekyb3d8bbwe}
+catch {"==Provisioned package for 3D Viewer not found.=="}
+
+Write-Host "Removing provisioned package for Paint 3D"
+try {Remove-AppxProvisionedPackage -Online -PackageName Microsoft.MSPaint_2021.2105.4017.0_neutral_~_8wekyb3d8bbwe}
+catch {"==Provisioned package for Paint 3D not found.=="}
+
+Write-Host "Removing user package for 3D Viewer"
+try {Get-AppxPackage -allusers *3dviewer* | Remove-AppxPackage -allusers}
+catch {"==3D Viewer local app not found.=="}
+
+Write-Host "Removing user package for Paint 3D"
+try {Get-AppxPackage -allusers *paint* | Remove-AppxPackage -allusers}
+catch {"==3D Viewer local app not found.=="}
+
+# Check for any remaining apps
+Write-Host "Final Checks"
+Get-AppxProvisionedPackage -online | Where-Object {$_.displayname -Like "*3dviewer*"}
+Get-AppxProvisionedPackage -online | Where-Object {$_.displayname -Like "*paint*"}
+Get-AppxPackage -AllUsers | select name, packagefullname | Where-Object {$_.name -Like "*3dviewer*"}
+Get-AppxPackage -AllUsers | select name, packagefullname | Where-Object {$_.name -Like "*paint*"}
